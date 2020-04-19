519e5e80de543a8083da8e0244e471ffb84252d6
Anna Gingle <annagingle977@gmail.com>
2020-04-19 17:56:33 -0500

    Modal added to about page
    Still need to customize content, style button, and style modal

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+//1 Way to Open, 2 to Close
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal 
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it == test to see if they are the same object
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
